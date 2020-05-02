8667f614ee639714c4fbf958348421b48f288f66
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-05-02 20:34:58 +0300

    edit route

--- src/components/todo-list.component.js
+++ src/components/todo-list.component.js
@@ -4,9 +4,9 @@ import axios from "axios";
 
 const Todo = (props) => (
   <tr>
-    <td>{props.todo.todo_description}</td>
-    <td>{props.todo.todo_responsible}</td>
-    <td>{props.todo.todo_priority}</td>
+    <td className={props.todo.todo_completed ? "completed" : ""}>{props.todo.todo_description}</td>
+    <td className={props.todo.todo_completed ? "completed" : ""}>{props.todo.todo_responsible}</td>
+    <td className={props.todo.todo_completed ? "completed" : ""}>{props.todo.todo_priority}</td>
     <td>
       <Link to={"/edit/" + props.todo._id}>Edit</Link>
     </td>
@@ -29,6 +29,16 @@ export default class TodoList extends Component {
       });
   }
 
+  componentDidUpdate() {
+    axios
+      .get("http://localhost:4000/todos/")
+      .then((response) => {
+        this.setState({ todos: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
   todoList() {
     return this.state.todos.map(function (currentTodo, i) {
       return <Todo todo={currentTodo} key={i} />;
